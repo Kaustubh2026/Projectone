ccf5151d9638e9244e27f01769c7d38331bef213
Kaustubh Muley <mkaustubh2026@gmail.com>
2025-05-20 15:22:22 +0530

    .env changed

--- backend/server.js
+++ backend/server.js
@@ -11,10 +11,11 @@ app.use(express.json());
 
 // Database connection
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'NN',
+  host: process.env.RAILWAY_MYSQL_HOST || 'mysql.railway.internal',
+  user: process.env.RAILWAY_MYSQL_USER || 'root',
+  password: process.env.RAILWAY_MYSQL_PASSWORD || 'GesuTyySLuWmURoslFCWLcnUlTfFsiSc',
+  database: process.env.RAILWAY_MYSQL_DATABASE || 'railway',
+  port: process.env.RAILWAY_MYSQL_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
